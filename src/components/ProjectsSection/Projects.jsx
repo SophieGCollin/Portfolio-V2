8e57cfe4eee601e4d32fdda3e33861883ea4718e
Sophie G. Collin <sophiecollin@Sophies-MacBook.local>
2020-06-04 16:56:34 -0400

    Projects Section Done

--- src/components/ProjectsSection/Projects.jsx
+++ src/components/ProjectsSection/Projects.jsx
@@ -4,7 +4,15 @@ import "./Projects.css";
 function Projects() {
   return (
     <div className="projects">
-      <h2>Projects</h2>
+      <h2 className="projectsHeading">
+        Projects <span className="punctuation">.</span>
+      </h2>
+      <div>
+        <p className="projectsText">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
     </div>
   );
 }
